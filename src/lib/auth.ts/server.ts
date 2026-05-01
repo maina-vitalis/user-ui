2abd164b5fc7c7fed36a035a6bf139f40cbd19ff
maina-vitalis <mainavitalis65@gmail.com>
2026-05-01 18:11:51 +0300

    feat: update Next.js version and refactor user authentication API call

--- src/lib/auth.ts/server.ts
+++ src/lib/auth.ts/server.ts
@@ -4,14 +4,11 @@ import { redirect } from "next/navigation";
 export const requireUser = async () => {
   const cookieStore = await cookies();
   const cookieHeader = cookieStore.toString();
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users/me`,
-    {
-      headers: { Cookie: cookieHeader },
-      credentials: "include",
-      cache: "no-store",
-    },
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/users`, {
+    headers: { Cookie: cookieHeader },
+    credentials: "include",
+    cache: "no-store",
+  });
 
   if (!res.ok) {
     return redirect("/auth/sign-in");
